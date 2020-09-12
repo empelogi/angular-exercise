a649e5659e71542f1b90a88f1d8e82e15bdbf82b
empelogi <belogiannieleni@gmail.com>
2020-09-12 22:20:47 +0300

    First commit of angular exercise

---
+++ src/app/countries/country-list/country-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../country';
+import { CountryService } from '../country.service';
+
+@Component({
+  selector: 'app-country-list',
+  templateUrl: './country-list.component.html',
+  styleUrls: ['./country-list.component.scss']
+})
+export class CountryListComponent implements OnInit {
+
+  title = 'My Countries';
+  countries: Country[];
+
+  constructor(private countryService : CountryService) { }
+
+  ngOnInit(): void {
+    this.countryService.getCountries().subscribe(countries => this.countries = countries );
+  }
+
+}
